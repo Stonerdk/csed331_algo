04b7f5c1c0fda96b08520a5d4297fdc5b8e434df
Stonerdk <56301345+Stonerdk@users.noreply.github.com>
2021-08-14 01:57:42 +0900

    코드만 남김

--- assn4/s.py
+++
@@ -1,3 +0,0 @@
-a = [(0, 1, 2), (2, 3, 4), (3, 4, 5)]
-for idx, (a, b, c) in enumerate(a):
-    print(idx, a, b, c)
--- midterm/final_preparations/lcs.py
+++
@@ -1,7 +0,0 @@
-S1 = input()
-S2 = input()
-dp = [[0 for _ in range(len(S2) + 1)] for _ in range(len(S1) + 1)]
-for i in range(len(S1)):
-    for j in range(len(S2)):
-            dp[i + 1][j + 1] = dp[i][j] + 1 if S1[i] == S2[j] else max(dp[i][j + 1], dp[i + 1][j])
-print(dp[-1][-1])
